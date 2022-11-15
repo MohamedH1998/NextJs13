3e890e60d27f18b72664cd626d8cb6483b74aa3c
Mohamed Hassan <mohamedhassan@Mohameds-Air.broadband>
2022-11-15 22:07:03 +0000

    Playing around with Next Js 13

---
+++ app/todos/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodosList from './TodosList'
+
+const Todos = () => {
+  return (
+    // <div>Todos</div>
+    <div>
+      <h1>This is where the todos will be listed</h1>
+    </div>
+  )
+}
+
+export default Todos
